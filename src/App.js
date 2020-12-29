536a5162ac318a1f327f440ce887355d8657b063
Koonkaduwa <adeepa91@gmail.com>
2020-12-29 21:48:02 +0530

    Layout added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { Layout } from 'antd';
+import 'antd/dist/antd.css';
+import { HeaderLayout } from './components/Header';
+import { Sidebar } from './components/Sidebar';
+import { ContentLayout } from './components/Content';
+
+const { Header, Footer, Sider, Content } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Sider>
+        <Sidebar />
+      </Sider>
+      <Layout>
+        <Header className="layout-header">
+          <HeaderLayout />
+        </Header>
+        <Content style={{ padding: '20px 20px 0px 20px' }}>
+          <ContentLayout />
+        </Content>
+        <Footer className="layout-footer">Footer</Footer>
+      </Layout>
+      
+    </Layout>
   );
 }
 
---
+++ src/components/Content.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../App.css'
+
+export const ContentLayout = () => {
+    return (
+        <div className="site-layout-content" style={{ padding: 24, minHeight: 590 }}>
+            Content
+        </div>
+    )
+}
